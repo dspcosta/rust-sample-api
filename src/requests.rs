d0ec49ceeff27bb7f4a3cab6400607d3d6bd2562
Daniel <daniel@host-7dl43ntpvb5rv4ytj.pd.osb-ldc1d1-a.v6.dfn.nl>
2026-05-02 17:08:09 +0200

    Add endpoints to fetch addresses and cities

--- src/requests.rs
+++ src/requests.rs
@@ -4,12 +4,12 @@ use axum::http::StatusCode;
 use diesel::prelude::*;
 
 use crate::DbPool;
+use crate::models::Address;
+use crate::models::City;
 use crate::models::Customer;
+use crate::schema::address::dsl::*;
+use crate::schema::city::dsl::*;
 use crate::schema::customer::dsl::*;
-
-pub async fn get_foo() -> &'static str {
-    "you have requested /get foo again\n"
-}
 
 pub async fn get_customers(
     Extension(pool): Extension<DbPool>,
@@ -29,6 +29,54 @@ pub async fn get_customers(
             (
                 StatusCode::INTERNAL_SERVER_ERROR,
                 format!("Error loading customers: {}", e),
+            )
+        })?;
+
+    Ok(Json(results))
+}
+
+pub async fn get_addresses(
+    Extension(pool): Extension<DbPool>,
+) -> Result<Json<Vec<Address>>, (StatusCode, String)> {
+    let mut conn = pool.get().map_err(|e| {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Failed to get connection: {}", e),
+        )
+    })?;
+
+    let results = address
+        .limit(1000)
+        .select(Address::as_select())
+        .load(&mut conn)
+        .map_err(|e| {
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Error loading addresses: {}", e),
+            )
+        })?;
+
+    Ok(Json(results))
+}
+
+pub async fn get_cities(
+    Extension(pool): Extension<DbPool>,
+) -> Result<Json<Vec<City>>, (StatusCode, String)> {
+    let mut conn = pool.get().map_err(|e| {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Failed to get connection: {}", e),
+        )
+    })?;
+
+    let results = city
+        .limit(1000)
+        .select(City::as_select())
+        .load(&mut conn)
+        .map_err(|e| {
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Error loading addresses: {}", e),
             )
         })?;
 
