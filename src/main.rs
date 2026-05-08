dd8b2788e1a75aad328aa98fd8f8648983a86a17
Daniel <dcostabus@gmail.com>
2026-05-08 18:24:21 +0200

    Add db pool size and max lifetime configuration

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,12 @@
+use std::time::Duration;
+
 use axum::{Router, extract::Extension, routing::get};
 
+mod db;
 mod errors;
+mod handlers;
 mod models;
-mod requests;
 mod schema;
-
-use diesel::pg::PgConnection;
-use diesel::r2d2::{self, ConnectionManager};
-use dotenvy::dotenv;
-use std::env;
-
-use crate::errors::AppError;
-
-// Create a type alias for connection pool
-pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
-
-/// Initializes the database connection pool from DATABASE_URL env var
-fn init_pool() -> Result<DbPool, AppError> {
-    dotenv().ok();
-
-    let database_url = env::var("DATABASE_URL").map_err(|e| {
-        AppError::InternalServerError(format!("DATABASE_URL must be set {}", e.to_string()))
-    })?;
-    let manager = ConnectionManager::<PgConnection>::new(database_url);
-
-    // Create a pool with 10 connections (default)
-    r2d2::Pool::builder()
-        .build(manager)
-        .map_err(AppError::PoolError)
-}
 
 #[tokio::main]
 async fn main() {
@@ -36,16 +14,17 @@ async fn main() {
         .format_timestamp_secs()
         .init();
 
-    let pool = init_pool().expect("Failed to initialize DB pool.");
+    let pool =
+        db::init_pool(10, Some(Duration::from_secs(600))).expect("Failed to initialize DB pool.");
 
     let app = Router::new()
         .route(
             "/",
-            get(|| async { "Hello from a simple demo API built with Rust and Axum!" }),
+            get(|| async { "Hello from a demo API built with Rust and Axum!" }),
         )
-        .route("/customers", get(requests::get_customers))
-        .route("/addresses", get(requests::get_addresses))
-        .route("/cities", get(requests::get_cities))
+        .route("/customers", get(handlers::customer::get_customers))
+        .route("/addresses", get(handlers::address::get_addresses))
+        .route("/cities", get(handlers::city::get_cities))
         .layer(Extension(pool));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
